058c25c45b51dc6be7bd3efb075adbfc83347e92
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-08-25 22:29:10 +0500

    Исправил тесты

--- utils/test.sh
+++ utils/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./text2dictionary.sh README.md README.md.test
-if cmp README.md.test README.md.dict; then
+./text2dictionary.sh ../README.md /tmp/README.md.test
+if cmp /tmp/README.md.test ../data/README.md.dict; then
 	echo Test ok C:
 else
 	echo Failed, please fix bug and pull request me :C
