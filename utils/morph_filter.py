a482dea42a4dec99dca44a15969b6487807aa15b
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-08-26 01:20:49 +0500

    Ещё немного упростил код

--- utils/morph_filter.py
+++ utils/morph_filter.py
@@ -1,12 +1,12 @@
 #coding:utf8
+# берём из файла argv[1] те слова, которые имеют все тэги argv[2:]
+
 from __future__ import print_function
 import pymorphy2, sys
 
 morph = pymorphy2.MorphAnalyzer()
-
 map(lambda w: print(w.word.encode('utf-8')),
     filter(lambda w: set(sys.argv[2:]) in w.tag,
     map(lambda w: morph.parse(w.strip().decode('utf-8'))[0],
-    filter(lambda s: len(s) > 1,
-    map(str.strip, open(sys.argv[1]).readlines()))))
+    map(str.strip, open(sys.argv[1]).readlines())))
 )
