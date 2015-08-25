5cc7ab0d03acad026c48264ec5bb0475763b0c13
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-08-26 00:59:01 +0500

    Небольшое упрощение

--- utils/morph_filter.py
+++ utils/morph_filter.py
@@ -1,13 +1,12 @@
-#!/opt/local/bin/python3.4
 #coding:utf8
 from __future__ import print_function
 import pymorphy2, sys
 
 morph = pymorphy2.MorphAnalyzer()
 
-map(lambda w: print(w[0].word.encode('utf-8')),
-    filter(lambda w: set(sys.argv[2:]) in w[0].tag,
-    map(lambda w: morph.parse(w.strip().decode('utf-8')),
+map(lambda w: print(w.word.encode('utf-8')),
+    filter(lambda w: set(sys.argv[2:]) in w.tag,
+    map(lambda w: morph.parse(w.strip().decode('utf-8'))[0],
     filter(lambda s: len(s) > 1,
     map(str.strip, open(sys.argv[1]).readlines()))))
 )
